e93ff99a3a3bf27694d6fa0b6d66dae5cb651d0c
Asiryan Valeriy <50625308+asiryan@users.noreply.github.com>
2020-11-17 01:59:05 +0300

    howbeautifulami

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # HowBeautifulAmI
 Python application for face detection and prediction age, gender and face beauty.
 
-## Technique
-As you know, the gender, age and beauty of a person is determined by his face. After detecting a face to obtain additional information about the face (hair, neck, ears, etc.), the selected area is artificially expanded while maintaining proportions. Then the expanded area of the face is cut into a separate image, to which convolutional neural networks are applied in turn to predict the gender, age and beauty of the face.
-<p align="center"><img width="100%" src="docs/technique_a.jpg" /></p>
-
 ## Dependencies
 Install python 3+ dependencies
 - opencv-python  
@@ -36,7 +32,7 @@ beauty_model = "models/beauty_resnet18.onnx"
 ```
 usage: python howbeautifulami.py [-i] --input_image
 ```
-<p align="center"><img width="40%" src="docs/charlize.jpg"/><img width="40%" src="docs/michael.jpg"/></p>   
+<p align="center"><img width="40%" src="examples/charlize.jpg"/><img width="40%" src="examples/michael.jpg"/></p>   
 
 Results for **Charlize Theron** photo
 ```
--- howbeautifulami.py
+++ howbeautifulami.py
@@ -1,122 +1,124 @@
 import cv2
-import numpy as np
-import onnxruntime
-from utils.box_utils import predict
-from utils.image_utils import resizeImage, cropImage, scaleRectangle
+import math
 import argparse
+import numpy as np
 
-# onnx models and params
-face_model = "models/face_detector_ultra_light_640.onnx"
-age_model = "models/vgg_ilsvrc_16_age_imdb_wiki.onnx"
-gender_model = "models/vgg_ilsvrc_16_gender_imdb_wiki.onnx"
-beauty_model = "models/beauty_resnet18.onnx"
-factor = 0.5
+# detect face
+def highlightFace(net, frame, conf_threshold=0.95):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
-# graphics 
-font = cv2.FONT_HERSHEY_SIMPLEX
-org = (50, 50)
-fontScale = 1
-color = (255, 0, 0)
-thickness = 2
-point = (5, 50)
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
 
-# prepare gender, age and beauty inference sessions
-face_session = onnxruntime.InferenceSession(face_model)
-gender_session = onnxruntime.InferenceSession(gender_model)
-age_session = onnxruntime.InferenceSession(age_model)
-beauty_session = onnxruntime.InferenceSession(beauty_model)
+# resize image with original ratio
+def resizeImage(img, size):
+    jpg = img
+    shape = jpg.shape[:2]
+    r = min(size[0] / shape[0], size[1] / shape[1])
+    new_size = int(round(shape[0] * r)), int(round(shape[1] * r))
+    border = int((size[0] - new_size[0]) / 2), int((size[1] - new_size[1]) / 2)
+    jpg = cv2.resize(jpg, (new_size[1], new_size[0]))
+    num = np.zeros((size[0], size[1], 3), np.uint8) + 255
+    num[border[0]:new_size[0]+border[0], border[1]:new_size[1]+border[1]] = jpg 
+    return num
 
-# face detection
-def faceDetection(raw_img):
-    input_name = face_session.get_inputs()[0].name
-    h, w, _ = raw_img.shape
-    img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (640, 480))
-    img_mean = np.array([127, 127, 127])
-    img = (img - img_mean) / 128
-    img = np.transpose(img, [2, 0, 1])
-    img = np.expand_dims(img, axis=0)
-    img = img.astype(np.float32)
-    confidences, boxes = face_session.run(None, {input_name: img})
-    boxes, labels, probs = predict(w, h, confidences, boxes, 0.7)
-    return boxes, labels, probs
+# crop image function
+def cropImage(image, c1, c2):
+    num = np.zeros((c2[0]-c1[0], c2[1]-c1[1], 3), np.uint8)
+    num = image[c1[0]:c2[0], c1[1]:c2[1]]
+    return num
 
-# face beauty
-def faceBeauty(raw_img):
-    img = resizeImage(raw_img, (224, 224))
-    img_mean = np.array([104, 117, 123])
-    img = (img - img_mean) / 255
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = img.astype(np.float32)
-    img = np.expand_dims(img, axis=0)
-    input_name = beauty_session.get_inputs()[0].name
-    outputs = beauty_session.run(None, {input_name: img})[0][0]
-    beauty = round(2.0 * sum(outputs), 1)
-    return beauty
+# scale reactangle function
+def scaleRectangle(shape, c1, c2, factor = 1):
+    squareImage = shape[0] * shape[1]
+    squareRectangle = (c2[0] - c1[0]) * (c2[1] - c1[1])
+    gain = (squareRectangle / squareImage)**factor
+    dx = int(c1[0] * gain)
+    dy = int(c1[1] * gain)
+    do = min(dx, dy)
+    c1 = c1[0] - do, c1[1] - do
+    c2 = c2[0] + do, c2[1] + do
+    return c1, c2
 
-# face gender
-def faceGender(raw_img):
-    img = resizeImage(raw_img, (224, 224))
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = img.astype(np.float32)
-    img = np.expand_dims(img, axis=0)
-    input_name = gender_session.get_inputs()[0].name
-    outputs = gender_session.run(None, {input_name: img})[0][0]
-    gender = 'Woman' if (np.argmax(outputs) == 0) else 'Man'
-    return gender
-
-# face age
-def faceAge(raw_img):
-    img = resizeImage(raw_img, (224, 224))
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = img.astype(np.float32)
-    img = np.expand_dims(img, axis=0)
-    input_name = age_session.get_inputs()[0].name
-    outputs = age_session.run(None, {input_name: img})[0][0]
-    age = round(sum(outputs * list(range(0, 101))), 1)
-    return age
-
-# parse args
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--image", type=str, required=False, help="input image")
-    args = parser.parse_args()
-    return args
 
 # main
-def main(args):
-    # load image
-    image_name = args.image
-    if image_name is None: image_name = "images/charlize.jpg"
-    image = cv2.imread(image_name)
-    shape = image.shape
-    print(f'Image: {image_name}')
+parser=argparse.ArgumentParser()
+parser.add_argument("-i", "--image", type=str, required=False, help="input image")
+args=parser.parse_args()
 
-    # face detection
-    boxes, labels, probs = faceDetection(image)
-    print(f'Detected faces: {boxes.shape[0]}\n------------------')
+faceProto="models/opencv_face_detector.pbtxt"
+faceModel="models/opencv_face_detector_uint8.pb"
+ageProto="models/age_googlenet.prototxt"
+ageModel="models/age_googlenet.caffemodel"
+genderProto="models/gender_googlenet.prototxt"
+genderModel="models/gender_googlenet.caffemodel"
+beautyProto="models/beauty_resnet.prototxt"
+beautyModel="models/beauty_resnet.caffemodel"
 
-    # face analytics
-    for i in range(boxes.shape[0]):
-        box = boxes[i, :]
-        c1, c2 = (box[0], box[1]), (box[2], box[3])
-        c1, c2 = scaleRectangle(shape, c1, c2, factor)
-        img = cropImage(image, (c1[1], c1[0]), (c2[1], c2[0]))
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+factor = 0.5
 
-        gender = faceGender(img)
-        age = faceAge(img)
-        beauty = faceBeauty(img)
-        labels = 'Gender: {}'.format(gender) + '\n' + 'Age: {}'.format(age) + '\n' + 'Beauty: {}'.format(beauty)
-        print(f'{labels}\n------------------\n')
-        
-        for i, label in enumerate(labels.split('\n')):
-            y = point[1] + i * point[1]
-            cv2.putText(img, label, (point[0], y), font, fontScale, color, thickness, cv2.LINE_AA)
-            cv2.rectangle(image, c1, c2, color, thickness=2)
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+beautyNet=cv2.dnn.readNet(beautyModel,beautyProto)
 
-    # save produced image
-    cv2.imwrite("processed.jpg", image)
-
-if __name__ == '__main__':
-    main(parse_args())
+video=cv2.VideoCapture(args.image if args.image else 0)
+padding=20
+while cv2.waitKey(1)<0 :
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
     
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
+
+    for faceBox in faceBoxes:
+
+        # image cropping
+        c1, c2 = scaleRectangle(frame.shape, (faceBox[1], faceBox[0]), (faceBox[3], faceBox[2]), factor)
+        face = cropImage(frame, c1, c2)
+        face = resizeImage(face, (224, 224))
+        ##cv2.imshow("Cropped face", face)
+
+        # gender net
+        blob=cv2.dnn.blobFromImage(face, 1.0, (224,224), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
+
+        # age net
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
+
+        # beauty net
+        blob=cv2.dnn.blobFromImage(face, 1.0/255, (224,224), MODEL_MEAN_VALUES, swapRB=False)
+        beautyNet.setInput(blob)
+        beautyPreds=beautyNet.forward()
+        beauty=round(2.0 * sum(beautyPreds[0]), 1)
+        print(f'Beauty: {beauty}/10.0')
+
+        cv2.putText(resultImg, f'{gender}, {age}, {beauty}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        cv2.imshow("howbeautifulami", resultImg)
+        cv2.imwrite("howbeautifulami.jpg", image)
