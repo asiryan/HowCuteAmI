de15c898f22e850ce6c8f757c09960fa0f42997b
asiryan <dmc5mod@yandex.ru>
2020-12-17 19:01:07 +0300

    howbeautifulami

--- howbeautifulami.py
+++ howbeautifulami.py
@@ -21,40 +21,25 @@ def highlightFace(net, frame, conf_threshold=0.95):
             y1=int(detections[0,0,i,4]*frameHeight)
             x2=int(detections[0,0,i,5]*frameWidth)
             y2=int(detections[0,0,i,6]*frameHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), color, int(round(frameHeight/400)), 8)
-    return frameOpencvDnn,faceBoxes
+            faceBoxes.append(scale([x1,y1,x2,y2]))
+            
+    return faceBoxes
 
-# resize image with original ratio
-def resizeImage(img, size):
-    jpg = img
-    shape = jpg.shape[:2]
-    r = min(size[0] / shape[0], size[1] / shape[1])
-    new_size = int(round(shape[0] * r)), int(round(shape[1] * r))
-    border = int((size[0] - new_size[0]) / 2), int((size[1] - new_size[1]) / 2)
-    jpg = cv2.resize(jpg, (new_size[1], new_size[0]))
-    num = np.zeros((size[0], size[1], 3), np.uint8) + 255
-    num[border[0]:new_size[0]+border[0], border[1]:new_size[1]+border[1]] = jpg 
+# scale current rectangle to box
+def scale(box):
+    width = box[2] - box[0]
+    height = box[3] - box[1]
+    maximum = max(width, height)
+    dx = int((maximum - width)/2)
+    dy = int((maximum - height)/2)
+    
+    bboxes = [box[0] - dx, box[1] - dy, box[2] + dx, box[3] + dy]
+    return bboxes
+
+# crop image
+def cropImage(image, box):
+    num = image[box[1]:box[3], box[0]:box[2]]
     return num
-
-# crop image function
-def cropImage(image, c1, c2):
-    num = np.zeros((c2[0]-c1[0], c2[1]-c1[1], 3), np.uint8)
-    num = image[c1[0]:c2[0], c1[1]:c2[1]]
-    return num
-
-# scale reactangle function
-def scaleRectangle(shape, c1, c2, factor = 1):
-    squareImage = shape[0] * shape[1]
-    squareRectangle = (c2[0] - c1[0]) * (c2[1] - c1[1])
-    gain = (squareRectangle / squareImage)**factor
-    dx = int(c1[0] * gain)
-    dy = int(c1[1] * gain)
-    do = min(dx, dy)
-    c1 = c1[0] - do, c1[1] - do
-    c2 = c2[0] + do, c2[1] + do
-    return c1, c2
-
 
 # main
 parser=argparse.ArgumentParser()
@@ -70,10 +55,10 @@ genderModel="models/gender_googlenet.caffemodel"
 beautyProto="models/beauty_resnet.prototxt"
 beautyModel="models/beauty_resnet.caffemodel"
 
-MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+MODEL_MEAN_VALUES=(104, 117, 123)
+#MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList=['Male','Female']
-factor = 0.5
 color = (0,255,255)
 
 faceNet=cv2.dnn.readNet(faceModel,faceProto)
@@ -81,46 +66,38 @@ ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 beautyNet=cv2.dnn.readNet(beautyModel,beautyProto)
 
-video=cv2.VideoCapture(args.image if args.image else 0)
-padding=20
-while cv2.waitKey(1)<0 :
-    hasFrame,frame=video.read()
-    if not hasFrame:
-        cv2.waitKey()
-        break
+frame=cv2.imread(args.image if args.image else 'images/charlize.jpg')
     
-    resultImg,faceBoxes=highlightFace(faceNet,frame)
-    if not faceBoxes:
-        print("No face detected")
+faceBoxes=highlightFace(faceNet,frame)
+if not faceBoxes:
+    print("No face detected")
 
-    for faceBox in faceBoxes:
+for faceBox in faceBoxes:
 
-        # image cropping
-        c1, c2 = scaleRectangle(frame.shape, (faceBox[1], faceBox[0]), (faceBox[3], faceBox[2]), factor)
-        face = cropImage(frame, c1, c2)
-        face = resizeImage(face, (224, 224))
-        ##cv2.imshow("Cropped face", face)
+    # face detection net
+    face = cropImage(frame, faceBox)
+    face = cv2.resize(face, (224, 224))
 
-        # gender net
-        blob=cv2.dnn.blobFromImage(face, 1.0, (224,224), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds=genderNet.forward()
-        gender=genderList[genderPreds[0].argmax()]
-        print(f'Gender: {gender}')
+    # gender net
+    blob=cv2.dnn.blobFromImage(face, 1.0, (224,224), MODEL_MEAN_VALUES, swapRB=False)
+    genderNet.setInput(blob)
+    genderPreds=genderNet.forward()
+    gender=genderList[genderPreds[0].argmax()]
+    print(f'Gender: {gender}')
 
-        # age net
-        ageNet.setInput(blob)
-        agePreds=ageNet.forward()
-        age=ageList[agePreds[0].argmax()]
-        print(f'Age: {age[1:-1]} years')
+    # age net
+    ageNet.setInput(blob)
+    agePreds=ageNet.forward()
+    age=ageList[agePreds[0].argmax()]
+    print(f'Age: {age[1:-1]} years')
 
-        # beauty net
-        blob=cv2.dnn.blobFromImage(face, 1.0/255, (224,224), MODEL_MEAN_VALUES, swapRB=False)
-        beautyNet.setInput(blob)
-        beautyPreds=beautyNet.forward()
-        beauty=round(2.0 * sum(beautyPreds[0]), 1)
-        print(f'Beauty: {beauty}/10.0')
+    # beauty net
+    blob=cv2.dnn.blobFromImage(face, 1.0/255, (224,224), MODEL_MEAN_VALUES, swapRB=False)
+    beautyNet.setInput(blob)
+    beautyPreds=beautyNet.forward()
+    beauty=round(2.0 * sum(beautyPreds[0]), 1)
+    print(f'Beauty: {beauty}/10.0')
 
-        cv2.putText(resultImg, f'{gender}, {age}, {beauty}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1.25, color, 2, cv2.LINE_AA)
-        cv2.imshow("howbeautifulami", resultImg)
-        cv2.imwrite("howbeautifulami.jpg", resultImg)
+    cv2.rectangle(frame, (faceBox[0],faceBox[1]), (faceBox[2],faceBox[3]), color, int(round(frame.shape[0]/400)), 8)
+    cv2.putText(frame, f'{gender}, {age}, {beauty}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1.25, color, 2, cv2.LINE_AA)
+    cv2.imshow("howbeautifulami", frame)
